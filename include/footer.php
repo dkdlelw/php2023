d5770cfc7197c36368359e81de8c9349450c8769
dkdlelw <1346zany@gmail.com>
2023-05-23 09:23:00 +0900

    0523

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
     <div class="footer__inner btStyle mb100 container">
         <div>Copyright @ 2023 seungyeon</div>
-        <div>blog by seungyeon</div>
+        <div><a href="adminAgree.html" class="btnStyle5">관리자 로그인</a><a href="mailto:1346zany@gmail.com">blog by seungyeon</a></div>
     </div>
 </footer>
     <!-- //footer -->
